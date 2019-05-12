5ed21b2eab2a9eda2a544baae49053b4e216a525
patomation <patomation@gmail.com>
2019-05-12 12:01:45 +0700

    changes tape color to pink

--- src/components/cassette-tape/cassette-tape.js
+++ src/components/cassette-tape/cassette-tape.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-import face from './face.png';
+import face from './face-pink.png';
 import spindle from './spindle.png';
 import style from './cassette-tape.scss';
 
